134f1e315dccab3bce1a7a66b1f6830d52d38397
aimwadee <aimwadee2010@gmail.com>
2024-08-28 12:20:12 +0700

    finished excercise

--- ex-2.js
+++ ex-2.js
@@ -6,5 +6,11 @@ const product = {
   ratings: 2.5,
   photo: null,
 };
-let socialMediaShare = "facebookShare";
+product.ratings = 4.5
+product.photo = https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg
+product[code] = IT0101010
+product.price = 6000
+delete product.ratings
+
+let socialMediaShare = 4.5;
 // Start code here
